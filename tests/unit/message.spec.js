442fa85fa374a454f3e2546e8fa40fe8332247cd
Pedro Yaya <pedro.y@floraqueen.com>
2021-05-10 11:43:55 +0200

    unit tests 1

---
+++ tests/unit/message.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils'
+import Message from '@/components/Message.vue'
+
+describe('Message.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    const date = '23 feb'
+    const wrapper = shallowMount(Message, {
+      propsData: {
+        message: {
+          msg: msg,
+          date: date
+        }
+      }
+    })
+    expect(wrapper.vm.$props.message.msg).toBe(msg)
+    expect(wrapper.vm.$props.message.date).toBe(date)
+  })
+})
